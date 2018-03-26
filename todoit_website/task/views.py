ebb651ebc5a5a8059634bfb129d375e04dbeb69f
Kevin <deleok@rpi.edu>
2018-03-26 15:57:22 -0400

    updated django to include most recent homepage. also reorganized general structure of html and related files.

--- todoit_website/task/views.py
+++ todoit_website/task/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 #from django.template import loader
 
 # Create your views here.
-def index(request):
-   return render(request,'task/index.html')
+def home(request):
+   return render(request,'task/home.html')
