0add30cf8c34879c6ef63c088761f22fcfbcbb3e
Kevin <deleok@rpi.edu>
2018-03-27 17:24:55 -0400

    Starts at login and then goes to home

--- todoit_website/task/urls.py
+++ todoit_website/task/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-   path('', views.home, name='home'),
-   path('login', views.login, name='login'),
+   path('home', views.home, name='home'),
+   path('', views.login, name='login'),
 ]
 
