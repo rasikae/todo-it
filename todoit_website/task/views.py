eb68cd0d91ea35b2cc8d9d521296620635157bd9
Justin Mahoney <mahonj7@rpi.edu>
2018-03-28 21:44:03 +0000

    Add projects works

--- todoit_website/task/views.py
+++ todoit_website/task/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 #from django.template import loader\\
 from django.http import HttpResponseRedirect
 from .forms import taskform
+from .forms import projectform
 from .models import Task
+from .models import Project
 # Create your views here.
 
 def login(request):
@@ -12,24 +14,45 @@ def home(request):
     # if this is a POST request we need to process the form data
     if request.method == 'POST':
         # create a form instance and populate it with data from the request:
-        #if projectsubmit
-        form = taskform(request.POST)
-        # check whether it's valid:
-        if form.is_valid():
-            # process the data in form.cleaned_data as required
-            # ...
-            # redirect to a new URL:
-            newtask=Task(title=form.cleaned_data['title'])
-            newtask.do_date=form.cleaned_data['dodate']
-            newtask.due_date=form.cleaned_data['duedate']
-            newtask.progress=form.cleaned_data['progress']
-            newtask.desc=form.cleaned_data['desc']
-            newtask.save()
-            results=Task.objects.all()
-            return render(request, 'task/home.html', {'tasks':results,'form': form})
-
+        if 'tasksubmit' in request.POST:
+            form = taskform(request.POST)
+            form2 = projectform()
+            # check whether it's valid:
+            if form.is_valid():
+                # process the data in form.cleaned_data as required
+                # ...
+                # redirect to a new URL:
+                newtask=Task(title=form.cleaned_data['title'])
+                newtask.do_date=form.cleaned_data['dodate']
+                newtask.due_date=form.cleaned_data['duedate']
+                newtask.progress=form.cleaned_data['progress']
+                newtask.desc=form.cleaned_data['desc']
+                newtask.save()
+                tasks=Task.objects.all()
+                projects=Project.objects.all()
+                return render(request, 'task/home.html', {'tasks':tasks,'projects':projects,'form': form, 'form2':form2})
+        elif 'projectsubmit' in request.POST:
+            form = taskform()
+            form2 = projectform(request.POST)
+            # check whether it's valid:
+            if form2.is_valid():
+                # process the data in form.cleaned_data as required
+                # ...
+                # redirect to a new URL:
+                newpro=Project(title=form2.cleaned_data['title'])
+                newpro.due_date=form2.cleaned_data['duedate']
+                newpro.save()
+                tasks=Task.objects.all()
+                projects=Project.objects.all()
+                return render(request, 'task/home.html', {'tasks':tasks,'projects':projects,'form': form, 'form2':form2})
+            
+            
     # if a GET (or any other method) we'll create a blank form
     else:
         form = taskform()
-    results=Task.objects.all()
-    return render(request, 'task/home.html', {'tasks':results, 'form': form})
+        form2 = projectform()
+    form = taskform()
+    form2 = projectform()
+    tasks=Task.objects.all()
+    projects=Task.objects.all()
+    return render(request, 'task/home.html', {'tasks':tasks,'projects':projects,'form': form, 'form2':form2})
