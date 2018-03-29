8ebb63b5acb1bbb900972e97a55726f58c04206b
Rasika Ekhalikar <rekhalikar@gmail.com>
2018-03-29 02:44:29 +0000

    login

--- todoit_website/task/static/task/js/login.js
+++ todoit_website/task/static/task/js/login.js
@@ -1,15 +1,19 @@
-// alert("Today is");
-
+// <script type="text/javascript" src="http://code.jquery.com/jquery-1.7.1.min.js"></script>
 // $("loginid").onclick = function(){alert("clicked")};
-
+/* global $ */
 document.getElementById("loginid").onclick = function(){
 	// alert("clicked");
 	document.getElementById("loginfid").action("{% url 'home' %}");
 };
 
-document.getElementById("mID").onclick = function(){
-	alert("mID clicked");
-	document.getElementById("formid").animate({height: "toggle", opacity: "toggle"}, "slow");
+document.getElementById("messageID").onclick = function(){
+	// alert("alert.id");
+	var x = document.getElementById("form");
+	alert("alert.id");
+	x.classList.toggle("form");
+	// x.animate({height: "toggle", opacity: "toggle"}, "slow"); //should 
+	alert("alert.id");
+
 };
 
 
