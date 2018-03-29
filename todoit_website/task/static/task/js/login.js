25e3bf14762e238d8f5c40c1cc4010fbdb0f9807
Kevin <deleok@rpi.edu>
2018-03-29 00:11:32 -0400

    request more info at registration

--- todoit_website/task/static/task/js/login.js
+++ todoit_website/task/static/task/js/login.js
@@ -1,8 +1,4 @@
-// <script type="text/javascript" src="http://code.jquery.com/jquery-1.7.1.min.js"></script>
-// $("loginid").onclick = function(){alert("clicked")};
-/* global $ */
 document.getElementById("loginid").onclick = function(){
-	// alert("clicked");
 	document.getElementById("loginfid").action("{% url 'home' %}");
 };
 
@@ -14,14 +10,10 @@ document.getElementById("signinID").onclick = function(){
 	l.style.display = "block";
 };
 
-document.getElementById("createID").onclick = function(){
+document.getElementById("registerID").onclick = function(){
 	var r = document.getElementById("register");
 	var l = document.getElementById("loginfid");
 
 	l.style.display = "none"
 	r.style.display = "block";
 };
-
-// $('.message a').click(function(){
-//    $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
-// });
