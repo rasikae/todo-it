fe093686727bab1062bc2a13dba72b79c2641759
Rasika Ekhalikar <rekhalikar@gmail.com>
2018-04-15 21:18:09 +0000

    Click out of collaborators and edit task

--- todoit_website/task/static/task/js/home.js
+++ todoit_website/task/static/task/js/home.js
@@ -2,6 +2,8 @@
 var modal = document.getElementById('id01');
 var modal2 = document.getElementById('id02');
 var modal3 = document.getElementById('id03');
+var modal4 = document.getElementById('id04');
+var modal5 = document.getElementById('id05');
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
@@ -13,6 +15,12 @@ window.onclick = function(event) {
     }
     if(event.target == modal3){
         modal3.style.display = "none";
+    }
+    if(event.target == modal4){
+        modal4.style.display = "none";
+    }
+    if(event.target == modal5){
+        modal5.style.display = "none";
     }
 };
 
